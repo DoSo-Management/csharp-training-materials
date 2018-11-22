4d13338d3a2674f24dca9c1bdf9cb9757ca6c788
fluffyBunny <giorgi.tsouvaltzis@doso.ge>
2018-11-22 14:41:55 +0400

    Revert "Revert "Update Program.cs""
    This reverts commit 06918244b93235acde5e3a3f6469748cd669d288.

--- GeometricFiguresCalculator/GeometricFiguresCalculator/Program.cs
+++ GeometricFiguresCalculator/GeometricFiguresCalculator/Program.cs
@@ -10,6 +10,187 @@ namespace GeometricFiguresCalculator
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to Console's Geometric Figures Calculator.");
+            Console.WriteLine("Choose between these options to calculate: ");
+            Console.WriteLine("1.Triangle");
+            Console.WriteLine("2.Rectangle");
+            Console.WriteLine("3.Parallelogram");
+            ConsoleKeyInfo consoleKey = Console.ReadKey();
+            if (consoleKey.Key == ConsoleKey.D1)
+            {
+                Console.WriteLine();
+                Console.Write("Enter side A: ");
+                var sideA = Double.Parse(Console.ReadLine());
+                Console.Write("Enter side B: ");
+                var sideB = Double.Parse(Console.ReadLine());
+                Console.Write("Enter side C: ");
+                var sideC = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Do you want to Calculate Triangle's Area or Perimeter?: ");
+                Console.WriteLine("1.Perimeter");
+                Console.WriteLine("2.Area");
+                consoleKey = Console.ReadKey();
+                if (consoleKey.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine();
+                    Triangle Triangle = new Triangle(sideA, sideB, sideC);
+                    Triangle.GetPerimeter();
+                    Console.ReadLine();
+                }
+                else if (consoleKey.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine();
+                    Console.Write("Enter height of Triangle: ");
+                    var height = Double.Parse(Console.ReadLine());
+                    Triangle Triangle = new Triangle(sideA, sideB, sideC, height);
+                    Triangle.GetArea();
+                    Console.ReadLine();
+                }
+
+            }else if (consoleKey.Key == ConsoleKey.D2)
+            {
+                Console.WriteLine();
+                Console.Write("Enter Length: ");
+                var length = Double.Parse(Console.ReadLine());
+                Console.Write("Enter Width: ");
+                var width = Double.Parse(Console.ReadLine());
+                var Rectangle = new Rectangle(length, width);
+                Console.WriteLine("Do you want to Calculate Rectangle's Perimeter or Area?: ");
+                Console.WriteLine("1.Perimeter");
+                Console.WriteLine("2.Area");
+                consoleKey = Console.ReadKey();
+                if (consoleKey.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine();
+                    Rectangle.GetPerimeter();
+                }
+                else if (consoleKey.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine();
+                    Rectangle.GetArea();  
+                }
+            }else if (consoleKey.Key == ConsoleKey.D3)
+            {
+                Console.WriteLine();
+                Console.Write("Enter Length: ");
+                var length = Double.Parse(Console.ReadLine());
+                Console.Write("Enter Width: ");
+                var width = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Do you want to Calculate Parallelogram's Perimeter or Area?: ");
+                Console.WriteLine("1.Perimeter");
+                Console.WriteLine("2.Area");
+                consoleKey = Console.ReadKey();
+                if (consoleKey.Key == ConsoleKey.D1)
+                {
+                    Console.WriteLine();
+                    Paralellogram Paralellogram = new Paralellogram(length, width);
+                    Paralellogram.GetPerimeter();
+                    Console.ReadLine();
+                } else if (consoleKey.Key == ConsoleKey.D2)
+                {
+                    Console.WriteLine();
+                    Console.Write("Enter Paralellogram's Height: ");
+                    var height = Double.Parse(Console.ReadLine());
+                    Paralellogram Paralellogram = new Paralellogram(length, width, height);
+                    Paralellogram.GetArea();
+                    Console.ReadLine();
+                }
+            } 
+        }
+    }
+
+   public interface GeometricFigures
+    {
+        void GetArea();
+        void GetPerimeter();
+    }
+
+    public class Triangle : GeometricFigures
+    {
+        double _sideA;
+        double _sideB;
+        double _sideC;
+        double _height;
+
+        public Triangle(double sideA, double sideB, double sideC)
+        {
+            _sideA = sideA;
+            _sideB = sideB;
+            _sideC = sideC;
+        }
+
+        public Triangle(double sideA, double sideB, double sideC, double height)
+        {
+            _sideA = sideA;
+            _sideB = sideB;
+            _sideC = sideC;
+            _height = height;
+        }
+       public void GetArea()
+        {
+            var Area = 1 / 2 * (_height * _sideC);
+            Console.WriteLine($"Triangle Area: {Area}");
+        }
+
+        public void GetPerimeter()
+        {
+            var Perimeter = _sideA + _sideB + _sideC;
+            Console.WriteLine($"Triangle Perimeter: {Perimeter}");
+
+        }
+        
+    }
+
+    class Rectangle : GeometricFigures
+    {
+        double _sideA; // Length
+        double _sideB; // Width
+        public Rectangle(double length, double width) 
+        {
+            _sideA = length;
+            _sideB = width;
+        }
+        public void GetArea()
+        {
+            var Area = _sideA * _sideB; // Length * Width
+            Console.WriteLine($"Rectangle's Area: {Area}");
+        }
+
+        public void GetPerimeter()
+        {
+            var Perimeter = (2 * _sideA) + (2 * _sideB); // (2 * Length) + (2 * Width)
+            Console.WriteLine($"Rectangle's Perimeter: {Perimeter}");
+        }
+    }
+
+    class Paralellogram
+    {
+        double _height;
+        double _length;
+        double _width;
+
+        public Paralellogram(double length, double width)
+        {
+            _length = length;
+            _width = width;
+        }
+        public Paralellogram(double length, double width,double height)
+        {
+            _length = length;
+            _width = width;
+            _height = height;
+        }
+
+        public void GetArea()
+        {
+            var Area = (_height * _length);
+            Console.WriteLine($"Parallelogram's Area: {Area}");
+        }
+
+        public void GetPerimeter()
+        {
+            var Perimeter = 2 * (_length + _width);
+            Console.WriteLine($"Parallelogram's Perimeter: {Perimeter}");
         }
     }
 }
+
